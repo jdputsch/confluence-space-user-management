a8aa3b9f1c2b78c1cece2d82e117617e098989e4
Andy Brook <javahollic@gmail.com>
2007-08-16 10:37:28 +0000

    remove unnecessary import
    git-svn-id: https://studio.plugins.atlassian.com/svn/SUSR/trunk@77618 2c54a935-e501-0410-bc05-97a93f6bca70

--- src/main/java/csum/confluence/permissionmgmt/util/ldap/osuser/OSUserParser.java
+++ src/main/java/csum/confluence/permissionmgmt/util/ldap/osuser/OSUserParser.java
@@ -41,12 +41,12 @@ import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.pdfbox.util.operator.SetCharSpacing;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import csum.confluence.permissionmgmt.util.ldap.ILdapEnvironmentProvider;
 import csum.confluence.permissionmgmt.util.ldap.LDAPException;
